d0809a1d41949abca01a8f6a67a6720c217313cd
SaniTheWay <sanidhyad16@gmail.com>
2022-02-28 22:54:55 +0530

    hashtable example

---
+++ hashtabe_ex.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    // declaring unordered map as KEY : VALUE pair
+    unordered_map<string, int> menu;
+
+    // entering data: using hashtable with O(1) operations
+    menu["dosa"] = 45;
+    menu["pizza"] = 445;
+    menu["coffee"] = 40;
+
+    // searching using .count() func.
+    string item;
+    cin >> item;
+
+    if (menu.count(item) == 1) // 1 for TRUE/present ||  0 for FALSE/absent
+    {
+        cout << item << " is present, "
+             << "with cost: " << menu[item] << endl;
+    }
+    else
+        cout << item << " is not present.";
+}
