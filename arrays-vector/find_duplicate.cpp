79a9ccb4a06c0422779dd1ace6d212518c7a681c
SaniTheWay <sanidhyad16@gmail.com>
2022-04-26 23:11:56 +0530

    finding duplicates

---
+++ arrays-vector/find_duplicate.cpp
@@ -0,0 +1,19 @@
+// **Question : find-the-duplicate-number
+// Question Link : https://leetcode.com/problems/find-the-duplicate-number/
+// Approach: https://leetcode.com/problems/find-the-duplicate-number/solution/
+// status : TC= O(nlogn), SC= O(1)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int CODE()
+{
+}
+
+int main()
+{
+    int;
+
+    return 0;
+}
